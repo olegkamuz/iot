885e81c9a141cd48e9db5fd17801bca976d8bb4c
olegkamuz@gmail.com <olegkamuz@gmail.com>
2024-03-10 12:46:32 +0200

    first commit

---
+++ src/main/java/org/example/iot/IotApplication.java
@@ -0,0 +1,13 @@
+package org.example.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotApplication.class, args);
+    }
+
+}
